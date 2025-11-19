ab421d659a7af8f320ae9c1912f1eb8bf2d356c8
fuji ren <devbp1556@gmail.com>
2025-11-19 09:35:54 +0700

    tes1

--- src/app/component/containerStore.tsx
+++ src/app/component/containerStore.tsx
@@ -2,20 +2,27 @@
 import React from 'react'
 import Image from "next/image";
 
-function Container(props) {
+interface ContainerProps {
+  gambar: string;
+  coin: string | number;
+  harga: string | number;
+}
+
+
+function Container({ gambar, coin, harga }: ContainerProps) {
   return (
     <div className='bg-indigo-950 w-[250px] h-[300px] mt-3 rounded-2xl flex flex-col justify-between items-center overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-200 ease-in-out'>
         <div className='flex-1 flex flex-col justify-center items-center'>
             <div className={`relative w-[100px] h-[100px]`}>
             <Image
-            src={props.gambar}
+            src={gambar}
             alt='LuckyCoin'
             fill
             className='object-contain'
             />
             </div>
             <div className='flex flex-row justify-center items-center mt-3'>
-                <p className='font-bold text-amber-400 mr-2 text-3xl'>{props.coin}</p>
+                <p className='font-bold text-amber-400 mr-2 text-3xl'>{coin}</p>
                 <div className='relative w-[20px] h-[20px]'>
                     <Image
                     src="/coin.webp"
@@ -27,7 +34,7 @@ function Container(props) {
             </div>
         </div>
         <div className='bg-amber-400 w-full h-24 flex justify-center items-center'>
-            <p className='font-bold text-4xl'>{props.harga}</p>
+            <p className='font-bold text-4xl'>{harga}</p>
         </div>
     </div>
   )
